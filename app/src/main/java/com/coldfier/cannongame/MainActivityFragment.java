742c193a61fdf29a23a18625a0078bfdf48fd699
Dmitry1892 <author>
2020-12-08 17:47:33 +0300

    Initial commit, works incorrect

---
+++ app/src/main/java/com/coldfier/cannongame/MainActivityFragment.java
@@ -0,0 +1,46 @@
+package com.coldfier.cannongame;
+
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MainActivityFragment extends Fragment {
+
+    private CannonView cannonView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        cannonView = (CannonView) view.findViewById(R.id.cannonView);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        getActivity().setVolumeControlStream(AudioManager.STREAM_MUSIC);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        cannonView.stopGame();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cannonView.releaseResources();
+    }
+}
